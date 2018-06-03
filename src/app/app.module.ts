b6d62a0f9c1023587b94ab91c9db41e8277127b6
jgascona <webcongas@gmail.com>
2018-06-03 18:16:49 +0200

    Seccion 13 clase 84

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import { routing, appRoutingProviders} from './app.routing';
 import { AppComponent } from './app.component';
 import { HomeComponent} from './components/home.component';
 import { ErrorComponent } from './components/error.component';
+import { ProductosListComponent } from './components/productos-list.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, ErrorComponent],
+  declarations: [AppComponent, HomeComponent, ErrorComponent, ProductosListComponent],
   imports: [BrowserModule, routing],
   providers: [appRoutingProviders],
   bootstrap: [AppComponent]
