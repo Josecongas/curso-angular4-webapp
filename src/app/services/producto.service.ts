99eda0557f8d9d814ebc2de0c7aceec364fa94ee
jgascona <webcongas@gmail.com>
2018-06-25 16:47:35 +0200

    Sección 13 clase 99

--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -7,49 +7,53 @@ import { GLOBAL } from './global';
 
 @Injectable()
 export class ProductoService {
-    public url: string;
+  public url: string;
 
-    constructor(
-        public _http: Http
-    ) {
-        this.url = GLOBAL.url;
-    }
+  constructor(public _http: Http) {
+    this.url = GLOBAL.url;
+  }
 
-    getProductos() {
-        // return 'TEXTO DESDE EL SERVICIO';
-        return this._http.get(this.url + 'productos').map(res => res.json());
-    }
+  getProductos() {
+    // return 'TEXTO DESDE EL SERVICIO';
+    return this._http.get(this.url + 'productos').map(res => res.json());
+  }
 
-    addProducto(producto: Producto) {
-        const json = JSON.stringify(producto);
-        const params = 'json=' + json;
-        const headers = new Headers({
-          'Content-Type': 'application/x-www-form-urlencoded'
-        });
+  getProducto(id) {
+    return this._http.get(this.url + 'producto/' + id).map(res => res.json());
+  }
 
-        return this._http.post(this.url + 'productos', params, {headers: headers}).map(res => res.json());
-    }
+  addProducto(producto: Producto) {
+    const json = JSON.stringify(producto);
+    const params = 'json=' + json;
+    const headers = new Headers({
+      'Content-Type': 'application/x-www-form-urlencoded'
+    });
 
-    makeFileRequest(url: string, params: Array<string>, files: Array<File>) {
-        return new Promise((resolve, reject) => {
-            const formData: any = new FormData();
-            const xhr = new XMLHttpRequest();
+    return this._http
+      .post(this.url + 'productos', params, { headers: headers })
+      .map(res => res.json());
+  }
 
-            for (let i = 0; i < files.length; i++) {
-                formData.append('uploads[]', files[i], files[i].name);
-            }
+  makeFileRequest(url: string, params: Array<string>, files: Array<File>) {
+    return new Promise((resolve, reject) => {
+      const formData: any = new FormData();
+      const xhr = new XMLHttpRequest();
 
-            xhr.onreadystatechange = function() {
-                if (xhr.readyState === 4) {
-                    if (xhr.status === 200) {
-                        resolve(JSON.parse(xhr.response));
-                    } else {
-                        reject(xhr.response);
-                    }
-                }
-            };
-            xhr.open('POST', url, true);
-            xhr.send(formData);
-        });
-    }
+      for (let i = 0; i < files.length; i++) {
+        formData.append('uploads[]', files[i], files[i].name);
+      }
+
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            resolve(JSON.parse(xhr.response));
+          } else {
+            reject(xhr.response);
+          }
+        }
+      };
+      xhr.open('POST', url, true);
+      xhr.send(formData);
+    });
+  }
 }
